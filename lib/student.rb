8b3dc6aa617a124c499b981fc2dcf54efc4dc9a1
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-21 02:22:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ class Student
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insterted_rowid() FROM ")
+    @id = DB[:conn].execute("SELECT last_insterted_rowid() FROM song")[0][0]
   end 
   
   def self.create
